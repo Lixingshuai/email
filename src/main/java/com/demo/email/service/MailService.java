b87675a32a781920269515c7bf68aa1812361271
lixingshuai <735543130@qq.com>
2019-01-12 19:43:14 +0800

    支持发送多人并抄送多人

--- src/main/java/com/demo/email/service/MailService.java
+++ src/main/java/com/demo/email/service/MailService.java
@@ -1,5 +1,43 @@
 package com.demo.email.service;
 
 public interface MailService {
-	void sendSimpleMail(String to, String subject, String content);
+
+	/**
+	 * 发送普通邮件
+	 * 
+	 * @param to      目标地址
+	 * @param subject 标题
+	 * @param content 内容
+	 */
+	void sendSimpleMail(String[] to, String[] cc, String subject, String content);
+
+	/**
+	 * 发送html格式邮件
+	 * 
+	 * @param to
+	 * @param subject
+	 * @param content
+	 */
+	void sendHtmlMail(String[] to, String[] cc, String subject, String content);
+
+	/**
+	 * 发送带附件的邮件
+	 * 
+	 * @param to
+	 * @param subject
+	 * @param content
+	 * @param filePath
+	 */
+	void sendAttachmentsMail(String[] to, String[] cc, String subject, String content, String filePath);
+
+	/**
+	 * 发送带静态资源的邮件
+	 * 
+	 * @param to
+	 * @param subject
+	 * @param content
+	 * @param rscPath
+	 * @param rscId
+	 */
+	void sendInlineResourceMail(String[] to, String[] cc, String subject, String content, String rscPath, String rscId);
 }
--- src/test/java/com/demo/email/service/MailServiceTest.java
+++ src/test/java/com/demo/email/service/MailServiceTest.java
@@ -15,6 +15,29 @@ public class MailServiceTest {
 
 	@Test
 	public void testSimpleMail() throws Exception {
-		MailService.sendSimpleMail("ityouknow@126.com", "test simple mail", " hello this is simple mail");
+		MailService.sendSimpleMail(new String[] { "3011131511@qq.com" }, null, "test simple mail",
+				" hello this is simple mail");
 	}
+
+	@Test
+	public void testHtmlMail() throws Exception {
+		String content = "<html>\n<body>\n    <h3>hello world ! 这是一封Html邮件!</h3>\n" + "</body>\n</html>";
+		MailService.sendHtmlMail(new String[] { "3011131511@qq.com" }, null, "test simple mail", content);
+	}
+
+	@Test
+	public void sendAttachmentsMail() {
+		String filePath = "e:\\tmp\\application.log";
+		MailService.sendAttachmentsMail(new String[] { "3011131511@qq.com" }, null, "主题：带附件的邮件", "有附件，请查收！", filePath);
+	}
+
+	@Test
+	public void sendInlineResourceMail() {
+		String rscId = "neo006";
+		String content = "<html><body>这是有图片的邮件：<img src=\'cid:" + rscId + "\' ></body></html>";
+		String imgPath = "C:\\Users\\summer\\Pictures\\favicon.png";
+		MailService.sendInlineResourceMail(new String[] { "3011131511@qq.com" }, null, "主题：这是有图片的邮件", content, imgPath,
+				rscId);
+	}
+
 }
